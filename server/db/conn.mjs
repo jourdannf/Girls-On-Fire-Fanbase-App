3e72751a11d2690a9de0282d8b6c98f93a5182e5
jourdannf <jourdann.fraser@gmail.com>
2024-08-09 14:34:43 -0400

    Server set up with successful connection to the database

---
+++ server/db/conn.mjs
@@ -0,0 +1,20 @@
+import dotenv from "dotenv";
+import { MongoClient } from "mongodb";
+
+const dbName = "kpopSurvivalShowDB";
+
+dotenv.config();
+
+const client = new MongoClient(process.env.ATLAS_URI);
+
+let conn;
+
+try {
+    conn = await client.connect(); 
+}catch(e) {
+    console.log(e);
+}
+
+let db = conn.db(dbName);
+
+export default db;
---
+++ server/index.js
@@ -0,0 +1,14 @@
+import db from "./db/conn.mjs"
+import express from "express";
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res)=> {
+    res.send("HEY");
+})
+
+app.listen(PORT, ()=> {
+    console.log(`Server is running on port ${PORT}...`);
+});
