711e935a6ba096d4a630af7a82d7f785c2ac6a6a
jourdannf <jourdann.fraser@gmail.com>
2024-08-12 17:27:05 -0400

    Finished model and routes for performances

---
+++ server/models/performance.js
@@ -0,0 +1,53 @@
+import mongoose, { mongo } from "mongoose";
+
+const scoreSchema = new mongoose.Schema({
+    type: {
+        type: String,
+        required: true
+    },
+    points: {
+        type: Map,
+        of: [Number],
+        required: true
+    }
+    
+});
+
+const performanceSchema = new mongoose.Schema({
+    show: {
+        type: String,
+        required: true,
+        unique: false
+    },
+    name: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    originalSong: {
+        type: Boolean,
+        required: true
+    },
+    artist: {
+        type: String,
+        required: true
+    },
+    round: {
+        type: Number,
+        require: true
+    },
+    participants: {
+        type: [String],
+        require:true
+    },
+    scores: {
+        type: scoreSchema,
+        require: true
+    },
+    link: {
+        type: String,
+        require: true
+    }
+}, {collection: "performances", strictQuery: "throw"});
+
+export default mongoose.model("Performance", performanceSchema);
--- server/routes/performances.js
+++ server/routes/performances.js
@@ -1,40 +1,39 @@
 import express from "express";
-import db from "../db/conn.mjs";
-import { ObjectId } from "mongodb";
+import mongoose from "mongoose";
+import Performance from "../models/performance.js";
 
 const router = express.Router();
 // const performances = db.collection("performances");
 
-// router
-//     .route("/")
-//     .get(async (req, res) => {
-//         const query = {show: "Girls On Fire"};
-//         let result = null;
+router
+    .route("/")
+    .get(async (req, res) => {
+        const query = {show: "Girls On Fire"};
 
-//         if (req.query.contestant){
-//             if (typeof req.query.contestant == "string"){
-//                 result = await performances.find({participants: {$in: [req.query.contestant]}}).toArray();
-//             }else {
-//                 result = await performances.find({participants: {$in: req.query.contestant}}).toArray(); //returns all performances containing either of contestants
-//             }
+        if (req.query.contestant){
+            // if (typeof req.query.contestant == "string"){
+            //     query.participants = {$in: req.query.contestant};
+            // }else {
+            //     query.participants = {$in: req.query.contestant}
+            // }
+            query.participants = {$in: req.query.contestant};
+        }
 
-//         }else {
-//             result = await performances.find(query).toArray();
-//         }
+        const result = await Performance.find(query);
 
-//         if (!result) res.status(404).send("Resource Not Found");
-//         else res.status(200).send(result);
-//     });
+        if (!result) res.status(404).send("Resource Not Found");
+        else res.status(200).send(result);
+    });
 
-// router
-//     .route("/:id")
-//     .get(async (req, res) => {
-//         const id = req.params.id;
-//         const query = {_id: new ObjectId(id)};
+router
+    .route("/:id")
+    .get(async (req, res) => {
+        const id = req.params.id;
+        // const query = {_id: new ObjectId(id)};
 
-//         const result = await performances.findOne(query).toArray();
-//         if (!result) res.status(404).send("Resource Not Found");
-//         else res.status(200).send(result);
-//     })
+        const result = await Performance.findById(id);
+        if (!result) res.status(404).send("Resource Not Found");
+        else res.status(200).send(result);
+    });
 
 export default router;
