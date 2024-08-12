dc345f8cd4890b8a3497b07456fca32c08bb717e
jourdannf <jourdann.fraser@gmail.com>
2024-08-12 17:46:33 -0400

    Finished contestants routes

--- server/routes/contestants.js
+++ server/routes/contestants.js
@@ -1,29 +1,31 @@
 import express from "express";
 import db from "../db/conn.mjs";
 import { ObjectId } from "mongodb";
+import mongoose from "mongoose";
+import Contestant from "../models/contestants.js"
 
 const router = express.Router();
 // const contestants = db.collection("contestants");
 
-// router
-//     .route("/")
-//     .get(async (req, res) => {
-//         const query = {show: "Girls On Fire"}
-//         const result = await contestants.find(query).toArray();
-//         if (!result) res.status(404).send("Resource Not Found");
-//         else res.status(200).send(result);
-//     });
+router
+    .route("/")
+    .get(async (req, res) => {
+        const query = {show: "Girls On Fire"}
+        const result = await Contestant.find(query);
+        if (!result) res.status(404).send("Resource Not Found");
+        else res.status(200).send(result);
+    });
 
-// router
-//     .route("/:id")
-//     .get(async (req, res) => {
-//         const id = req.params.id;
-//         const query = {_id: new ObjectId(id)};
+router
+    .route("/:id")
+    .get(async (req, res) => {
+        const id = req.params.id;
+        // const query = {_id: new ObjectId(id)};
 
-//         const result = await contestants.findOne(query);
+        const result = await Contestant.findById(id);
 
-//         if (!result) res.status(404).send("Resource Not Found");
-//         else res.status(200).send(result);
-//     })
+        if (!result) res.status(404).send("Resource Not Found");
+        else res.status(200).send(result);
+    })
 
 export default router;
