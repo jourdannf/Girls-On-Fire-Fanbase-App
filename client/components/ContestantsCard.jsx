c5b87a9995a169590b0a8c4d8e680c3691904b23
jourdannf <jourdann.fraser@gmail.com>
2024-08-14 17:06:02 -0400

    Added --host to package.json for client

--- client/components/ContestantsCard.jsx
+++ client/components/ContestantsCard.jsx
@@ -1,7 +1,7 @@
 export default function ContestantsCard ({contestant}) {
     return (
         <div>
-            <img src={contestant.img} alt={`Picture of ${contestant.name}`} />
+            <img src={contestant.img ? `../src/assets/${contestant.img}` : ""} alt={`Picture of ${contestant.name}`} />
             <p>{contestant.name}</p>
         </div>
     );
